d849437030ad3ae39a94413f135c5d1e2dd578a4
fabricio.demuner <fabricio.demuner@gmail.com>
2020-10-12 17:35:04 +0200

    toggle habit dates

--- frontend/src/HabitRow.js
+++ frontend/src/HabitRow.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Axios from "axios";
 import { API_HOST } from "./constants";
 
 const del = Axios.delete;
+const put = Axios.put;
 export default function HabitRow(props) {
   const { row, onChange } = props;
+  const today = JSON.stringify(new Date()).slice(1, 11);
+  const [date, setDate] = useState(today);
 
   async function handleDelete() {
     try {
       await del(`${API_HOST}/habit/${row.id}`);
+      onChange();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function toggleDate() {
+    try {
+      await put(`${API_HOST}/habit/${row.id}/days`, { date });
+      setDate(today);
       onChange();
     } catch (err) {
       console.log(err);
@@ -21,6 +34,8 @@ export default function HabitRow(props) {
         Name: {row.name}. Streak: {row.streak}.
       </p>
       <button onClick={handleDelete}>Delete Habit</button>
+      <input type="date" value={date} onChange={(e) => setDate(e.target.value)}></input>
+      <button onClick={toggleDate}>Toggle Date</button>
     </div>
   );
 }
